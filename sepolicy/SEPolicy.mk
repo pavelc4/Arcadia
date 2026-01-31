b38e56e087fdc2576cf4103f17b3bf0c21abb1c7
pavelc4 <dimzzy23@gmail.com>
2026-01-31 12:01:54 +0700

    build: Add SEPolicy inclusion makefile

---
+++ sepolicy/SEPolicy.mk
@@ -0,0 +1,10 @@
+# Arcadia Policy Config
+
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    packages/apps/Arcadia/sepolicy/vendor
+
+PRODUCT_PRIVATE_SEPOLICY_DIRS += \
+    packages/apps/Arcadia/sepolicy/private
+
+PRODUCT_PUBLIC_SEPOLICY_DIRS += \
+    packages/apps/Arcadia/sepolicy/public
